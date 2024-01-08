fab8c7f4fc7a01b46959b24a4fa9fb61e3acda8e
Marcos Paulo <marcos.paulocarneiro16@gmail.com>
2024-01-08 18:18:46 -0300

    Desenvolvendo módulo 1 - curto_circuito

---
+++ curto_circuito.py
@@ -0,0 +1,12 @@
+# CÓDIGO DO MÓDULO 1
+
+from funcionalidades import valores_base, obter_impedância
+
+corrente_base = valores_base() # CORRENTE BASE DO SISTEMA
+impedância_reduzida_positiva = obter_impedância() # IMPEDÂNCIA DE SEQUÊNCIA POSITIVA REDUZIDA DO SISTEMA
+impedância_reduzida_negativa = obter_impedância() # IMPEDÂNCIA DE SEQUÊNCIA POSITIVA REDUZIDA DO SISTEMA
+impedância_reduzida_zero = obter_impedância() # IMPEDÂNCIA DE SEQUÊNCIA ZERO REDUZIDA DO SISTEMA
+impedância_trafo_zero = obter_impedância() # IMPEDÂNCIA DE SEQUÊNCIA ZERO DO TRANSFORMADOR
+impedância_condutores_zero = obter_impedância() # IMPEDÂNCIA DE SEQUÊNCIA ZERO DOS CONDUTORES
+
+
