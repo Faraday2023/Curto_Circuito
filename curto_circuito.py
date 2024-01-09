e4a5c3675be95fbd942512cfb1d4d37e08edfbfb
Marcos Paulo <marcos.paulocarneiro16@gmail.com>
2024-01-08 21:43:22 -0300

    Implementação do cálculo da potência de curto-circuito e curto monofásico

--- curto_circuito.py
+++ curto_circuito.py
@@ -1,12 +1,19 @@
 # CÓDIGO DO MÓDULO 1
 
-from funcionalidades import valores_base, obter_impedância
+from funcionalidades import valores_base, obter_impedancia, corrente_curto_trifasica_simetrica,corrente_curto_monofasica
+from funcionalidades import potencia_de_curto_circuito
 
 corrente_base = valores_base() # CORRENTE BASE DO SISTEMA
-impedância_reduzida_positiva = obter_impedância() # IMPEDÂNCIA DE SEQUÊNCIA POSITIVA REDUZIDA DO SISTEMA
-impedância_reduzida_negativa = obter_impedância() # IMPEDÂNCIA DE SEQUÊNCIA POSITIVA REDUZIDA DO SISTEMA
-impedância_reduzida_zero = obter_impedância() # IMPEDÂNCIA DE SEQUÊNCIA ZERO REDUZIDA DO SISTEMA
-impedância_trafo_zero = obter_impedância() # IMPEDÂNCIA DE SEQUÊNCIA ZERO DO TRANSFORMADOR
-impedância_condutores_zero = obter_impedância() # IMPEDÂNCIA DE SEQUÊNCIA ZERO DOS CONDUTORES
 
+# IMPEDÂNCIAS 
+impedancia_reduzida_positiva = obter_impedancia() # IMPEDÂNCIA DE SEQUÊNCIA POSITIVA REDUZIDA DO SISTEMA
+'''impedancia_reduzida_negativa = obter_impedancia() # IMPEDÂNCIA DE SEQUÊNCIA POSITIVA REDUZIDA DO SISTEMA
+impedancia_reduzida_zero = obter_impedancia() # IMPEDÂNCIA DE SEQUÊNCIA ZERO REDUZIDA DO SISTEMA
+impedancia_trafo_zero = obter_impedancia() # IMPEDÂNCIA DE SEQUÊNCIA ZERO DO TRANSFORMADOR
+impedancia_condutores_zero = obter_impedancia() # IMPEDÂNCIA DE SEQUÊNCIA ZERO DOS CONDUTORES'''
 
+# CALCULANDO CURTO-CIRCUITO
+#tri = corrente_curto_trifasica_simetrica(impedancia_reduzida_positiva) # ICC TRIFÁSICO
+#print(tri)
+#mono = corrente_curto_monofásica(impedancia_reduzida_zero, impedancia_condutores_zero, impedancia_trafo_zero) # ICC MONO MÁX
+pot_cc = potencia_de_curto_circuito() # POTÊNCIA DE CURTO-CIRCUITO
--- funcionalidades.py
+++ funcionalidades.py
@@ -4,19 +4,26 @@ import math
 import numpy
 import cmath
 
-def obter_impedância():
+# VARIÁVEIS GLOBAIS
+
+corrente_base = None
+tensao = None
+potencia = None
+
+# FUNÇÃO PARA DETERMINAÇÃO DE IMPEDÂNCIAS
+def obter_impedancia():
     parte_real = float(input('Digite a parte real da impedância: '))
     parte_imaginaria = float(input('Digite a parte imaginária da impedância: '))
     return complex(parte_real, parte_imaginaria)
 
 # CORRENTE BASE
 def valores_base():
-    global corrente_base
-    potência = int(input('Defina a potência base: '))
-    tensão = int(input('Defina a tensão base: '))
+    global potencia, tensao, corrente_base
+    potencia = int(input('Defina a potência base: '))
+    tensao = int(input('Defina a tensão base: '))
 
     try:
-        corrente_base = potência / (tensão * math.sqrt(3))
+        corrente_base = potencia / (tensao * math.sqrt(3))
     except ZeroDivisionError:
         print("Erro: A tensão base não pode ser zero.")
         return None
@@ -24,18 +31,29 @@ def valores_base():
     return round(corrente_base, 2)
 
 # CORRENTE DE CURTO TRIFÁSICA SIMÉTRICA 
-def corrente_curto_trifásica_simétrica(impedância_reduzida_sistema):
+def corrente_curto_trifasica_simetrica(impedancia_reduzida_sistema):
 
     if corrente_base is None:
         return None  # Lidar com a situação de erro na corrente base
     
-    curto_trifásico_simétrico = (1/(impedância_reduzida_sistema)) * corrente_base
-    magnitude, angulo_radiano = cmath.polar(curto_trifásico_simétrico)
+    curto_trifasico_simetrico = (1/(impedancia_reduzida_sistema)) * corrente_base
+    magnitude, angulo_radiano = cmath.polar(curto_trifasico_simetrico)
     angulo_graus = math.degrees(angulo_radiano)
     return magnitude, angulo_graus
 
 # CORRENTE DE CURTO-CIRCUITO FASE/TERRA
-def corrente_curto_monofásica(impedância_reduzida,impedância_condutores,impedância_trafo):
+def corrente_curto_monofasica(impedancia_reduzida,impedancia_condutores,impedancia_trafo):
     
-    curto_fase_terra = (3 * corrente_base)/(2 *impedância_reduzida+impedância_condutores+impedância_trafo)
-    return curto_fase_terra
+    curto_fase_terra = (3 * corrente_base)/((2 *impedancia_reduzida)+impedancia_condutores+impedancia_trafo)
+    magnitude, angulo_radiano = cmath.polar(curto_fase_terra)
+    angulo_graus = math.degrees(angulo_radiano)
+    return magnitude, angulo_graus
+
+# POTÊNCIA DE CURTO-CIRCUITO
+
+def potencia_de_curto_circuito():
+    global tensao, corrente_base
+
+    potência_cc = math.sqrt(3) * tensao * corrente_base
+    
+    return round(potência_cc,2)
