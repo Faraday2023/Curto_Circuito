99d1dc105ef9a6668da490febab5dbea5e5cd8de
Marcos Paulo <marcos.paulocarneiro16@gmail.com>
2024-01-21 17:29:10 -0300

    Implementando função para cálculo de resistência e reatância do circuito alimentador

--- funcionalidades.py
+++ funcionalidades.py
@@ -8,6 +8,7 @@ import cmath
 
 corrente_base = None
 tensao = 13800
+tensao_secundaria = 380
 potencia = 100000000
 
 # FUNÇÃO DE MENU
@@ -20,6 +21,10 @@ def exibir_menu():
     print("3. Calcular Curto-Circuito-Trifásico-Simétrico")
     print("4. Calcular Curto-Circuito-Monofásico")
     print("5. Calcular Potência de Curto-Circuito")
+    print("6. Modelando transformador")
+    print("7. Calcular curto trifásico nos terminais do trafo")
+    print("8. Calcular curto monofásico nos terminais do trafo")
+    print("9. Calcular impedancia do alimentador")
     print("0. Sair")
     print('-'*40)
 
@@ -55,9 +60,9 @@ def corrente_curto_trifasica_simetrica(impedancia_reduzida_sistema):
     return magnitude, angulo_graus
 
 # CORRENTE DE CURTO-CIRCUITO FASE/TERRA
-def corrente_curto_monofasica(impedancia_reduzida,impedancia_condutores,impedancia_trafo):
+def corrente_curto_monofasica(impedancia_reduzida,impedancia_zero, impedancia_condutores,impedancia_trafo,corrente_base):
     
-    curto_fase_terra = (3 * corrente_base)/((2 *impedancia_reduzida)+impedancia_condutores+impedancia_trafo)
+    curto_fase_terra = (3 * corrente_base)/((2 *(impedancia_reduzida+impedancia_trafo))+ impedancia_zero + impedancia_condutores+impedancia_trafo)
     magnitude, angulo_radiano = cmath.polar(curto_fase_terra)
     angulo_graus = math.degrees(angulo_radiano)
     return magnitude, angulo_graus
@@ -163,16 +168,48 @@ def tipo_transformador():
             print('Opção inválida')
 
         resistencia_percentual = perdas_no_cobre/(10*int(pot_trafo))
-        resistencia_percentual_base = (resistencia_percentual/100)*((potencia/(int(pot_trafo)*1000))*(int(tensao_primaria)/tensao))
-        impedancia_percentual_base = (float(impedancia_de_placa)/100)*((potencia/(int(pot_trafo)*1000))*(int(tensao_primaria)/tensao))
+        resistencia_percentual_base = (resistencia_percentual/100)*((potencia/(int(pot_trafo)*1000))*(float(tensao_primaria)/tensao))
+        impedancia_percentual_base = (float(impedancia_de_placa)/100)*((potencia/(int(pot_trafo)*1000))*(float(tensao_primaria)/tensao))
         reatancia_percentual_base = math.sqrt((math.pow(impedancia_percentual_base,2)-math.pow(resistencia_percentual_base,2)))
-        print(resistencia_percentual_base)
 
         return complex(resistencia_percentual_base, reatancia_percentual_base)
+    
+def impedancia_alimentadores(comprimento_alimentador, cabos_por_fase):
+    global potencia, tensao_secundaria
 
-def curto_simetrico_transformador(suprimento, condutores, transformador):
-    tensao_secundaria = str(input('Digite a tensão no secundário do transformador: '))
-    corrente_base_secundaria = potencia/(math.sqrt(3)*tensao_secundaria)
-    impedancia_acumulada = suprimento + condutores + transformador
-    curto_simetrico_trafo = corrente_base_secundaria/impedancia_acumulada
+    dados_cabos = {
+        '1.5': {'resistencia_positiva': 14.8137, 'reatancia_positiva': 0.1378},
+        '2.5': {'resistencia_positiva': 8.8882, 'reatancia_positiva': 0.1345},
+        '4': {'resistencia_positiva': 5.5518, 'reatancia_positiva': 0.1279},
+        '6': {'resistencia_positiva': 3.7045, 'reatancia_positiva': 0.1225},
+        '10': {'resistencia_positiva': 2.221, 'reatancia_positiva': 0.1207},
+        '16': {'resistencia_positiva': 1.3899, 'reatancia_positiva': 0.1173},
+        '25': {'resistencia_positiva': 0.8891, 'reatancia_positiva': 0.1164},
+        '35': {'resistencia_positiva': 0.6353, 'reatancia_positiva': 0.1128},
+        '50': {'resistencia_positiva': 0.445, 'reatancia_positiva': 0.1127},
+        '70': {'resistencia_positiva': 0.3184, 'reatancia_positiva': 0.1096},
+        '95': {'resistencia_positiva': 0.2352, 'reatancia_positiva': 0.1090},
+        '120': {'resistencia_positiva': 0.1868, 'reatancia_positiva': 0.1076},
+        '150': {'resistencia_positiva': 0.1502, 'reatancia_positiva': 0.1076},
+        '185': {'resistencia_positiva': 0.1226, 'reatancia_positiva': 0.1073},
+        '240': {'resistencia_positiva': 0.0958, 'reatancia_positiva': 0.1070},
+        '300': {'resistencia_positiva': 0.0781, 'reatancia_positiva': 0.1068},
+        '400': {'resistencia_positiva': 0.0608, 'reatancia_positiva': 0.1058},
+        '500': {'resistencia_positiva': 0.0507, 'reatancia_positiva': 0.1051},
+        '630': {'resistencia_positiva': 0.0292, 'reatancia_positiva': 0.1042}
+    }
 
+    secao_cabo = input('Digite a seção nominal do cabo: ')
+    
+    if secao_cabo in dados_cabos:
+        dados = dados_cabos[secao_cabo]
+        resistencia_do_circuito = (dados['resistencia_positiva'] * comprimento_alimentador) / (1000 * cabos_por_fase)
+        reatancia_do_circuito = (dados['reatancia_positiva'] * comprimento_alimentador) / (1000 * cabos_por_fase)
+
+        resistencia_circuito_base_nova = (resistencia_do_circuito * 1000) * (potencia / (1000 * (tensao_secundaria ** 2)))
+        reatancia_circuito_base_nova = (reatancia_do_circuito * 1000) * (potencia / (1000 * (tensao_secundaria ** 2)))
+        impedancia_circuito = complex(resistencia_circuito_base_nova, reatancia_circuito_base_nova)
+
+        return resistencia_circuito_base_nova, reatancia_circuito_base_nova, impedancia_circuito
+    else:
+        print('Opção de cabo inválida')
